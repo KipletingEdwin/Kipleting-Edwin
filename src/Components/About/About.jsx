8ed0c8eecb1705c36b8df8735eaff6029f51a1f0
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-31 21:28:38 +0300

    add a div called about-left  with an image

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/about_profile.svg'
 
 const About = () => {
   return (
     <div className='about'>
         <div className="about-title">
             <h1>About Me</h1>
-            <img src={theme_pattern} alt="" />
+            <img src={theme_pattern} alt="theme" />
         </div>
         <div className='about-section'>
+            <div className="about-left">
+                <img src={profile_img} alt="profile" />
+            </div>
 
         </div>
     </div>
